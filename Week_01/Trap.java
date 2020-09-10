6e211ffde15cb80130a271f92285c3cc751fc8d5
Kung <geek.xm@gmail.com>
2020-09-11 02:57:47 +0800

    第一周作业

---
+++ Week_01/Trap.java
@@ -0,0 +1,29 @@
+public class Trap {
+
+    public int trap(int[] height) {
+        int sum = 0;
+        int max_left = 0;
+        int max_right = 0;
+        int left = 1;
+        int right = height.length - 2;
+        for (int i = 1; i < height.length - 1; i++) {
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                int min = max_left;
+                if (min > height[left]) {
+                    sum = sum + (min - height[left]);
+                }
+                left++;
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                int min = max_right;
+                if (min > height[right]) {
+                    sum = sum + (min - height[right]);
+                }
+                right--;
+            }
+        }
+        return sum;
+    }
+
+}
